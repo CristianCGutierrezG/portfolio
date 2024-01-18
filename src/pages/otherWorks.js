af9a6e749e4c56558e4742d3d17c53c95453686c
Cristian <cris123.gut@gmail.com>
2024-01-18 17:26:57 -0500

    Aesthetic change in the introduction, animation change in the coin and implementation of a new array.

---
+++ src/pages/otherWorks.js
@@ -0,0 +1,10 @@
+export default function OtherWorks(props) {
+    return (
+      <section id={props.id}>
+        <div className="otherWorks_background">
+            
+        </div>
+      </section>
+    );
+  }
+  
