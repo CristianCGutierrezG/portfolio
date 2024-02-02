c9340e3ba4c2b4e0ff7b3cc73d6009e9be08d710
Cristian <cris123.gut@gmail.com>
2024-02-02 18:13:16 -0500

    details Other Works

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,15 @@
+//images
+import bgCastle from "../images/Back-ground/BigSize/bgCastel.png";
+
 export default function Contact(props) {
   return (
     <section id={props.id}>
-      <div className="contact_background"></div>
+      <div className="contact_background"  
+        style={{
+          backgroundImage: `url(${bgCastle ? bgCastle : ""})`,
+        }}>
+
+          </div>
     </section>
   );
 }
